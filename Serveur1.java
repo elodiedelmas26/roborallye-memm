c5281a88a9c7eefd8f49116ee8e79267a4c98100
elodiedelmas26 <99487241+elodiedelmas26@users.noreply.github.com>
2022-02-17 08:44:34 +0100

    Update Serveur1.java

--- Serveur1.java
+++ Serveur1.java
@@ -6,35 +6,24 @@ import java.net.ServerSocket;
 
 public class Serveur1 {
 	 private static ServerSocket server;
-	    //socket server port on which it will listen
 	    private static int port = 9876;
 	    
 	    public static void main(String args[]) throws IOException, ClassNotFoundException{
-	        //create the socket server object
 	        server = new ServerSocket(port);
-	        //keep listens indefinitely until receives 'exit' call or program terminates
 	        while(true){
-	            System.out.println("Waiting for the client request");
-	            //creating socket and waiting for client connection
+	            System.out.println("En attente de la requete du client :");
 	            Socket socket = server.accept();
-	            //read from socket to ObjectInputStream object
 	            ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
-	            //convert ObjectInputStream object to String
 	            String message = (String) ois.readObject();
-	            System.out.println("Message Received: " + message);
-	            //create ObjectOutputStream object
+	            System.out.println("Message Recu: " + message);
 	            ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
-	            //write object to Socket
-	            oos.writeObject("Hi Client "+message);
-	            //close resources
+	            oos.writeObject("Bonjour Client ! "+message);
 	            ois.close();
 	            oos.close();
 	            socket.close();
-	            //terminate the server if client sends exit request
 	            if(message.equalsIgnoreCase("exit")) break;
 	        }
-	        System.out.println("Shutting down Socket server!!");
-	        //close the ServerSocket object
+	        System.out.println("Arrêt du serveur !");
 	        server.close();
 	    }
 	    
