654af3f47b02db7fdb6fbc2e1bf9edce85fdaf42
mathilde07380 <99496565+mathilde07380@users.noreply.github.com>
2022-03-24 18:17:01 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,49 @@
+package jeu;
+import javax.swing.*;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+
+import java.io.IOException;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class Main {
+	
+	public static JFrame fenetre;
+	public static Scene scene;
+	
+	
+	public static void main(String[] args) throws IOException {
+		
+		fenetre = new JFrame("RoboRally");//création de la fenetre
+		scene = new Scene();//création panneau
+	
+		
+		fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//permettre de fermer le programme avec la croix rouge
+		fenetre.setSize(1000,700); //dimension 
+		fenetre.setLocationRelativeTo(null);//centrer la fenetre à l'écran
+		fenetre.setResizable(false);//empeche de redimenssioné la fenetre
+		fenetre.setAlwaysOnTop(true);//Toujours au dessus des autres fenetres quand on joue
+		
+		fenetre.setContentPane(scene);
+		fenetre.setVisible(true);//fenetre visible
+		
+		JButton bouton1 = new JButton("Générer les cartes");
+		fenetre.getContentPane().add(bouton1);
+		fenetre.setVisible(true);
+		bouton1.setBounds(650, 50, 300, 50);
+
+		JButton bouton2 = new JButton("START");
+		fenetre.getContentPane().add(bouton2);
+		fenetre.setVisible(true);
+		bouton2.setBounds(650, 500, 300, 50);
+		
+		
+		
+
+	}
+
+}
