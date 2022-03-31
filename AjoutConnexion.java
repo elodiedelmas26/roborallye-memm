13ddf24beec6dd7f1b2b18f36eb9e3e66584c0cf
elodiedelmas26 <99487241+elodiedelmas26@users.noreply.github.com>
2022-03-31 18:34:05 +0200

    Add files via upload

---
+++ AjoutConnexion.java
@@ -0,0 +1,10 @@
+package code_serveur_client;
+
+import java.io.Serializable;
+
+public class AjoutConnexion implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	public int id;
+
+}
